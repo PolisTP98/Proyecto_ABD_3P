36f21663215fa4ef761d9f65ca9123eb38afe56f
M0chapaprika <yanyan2.0xd@gmail.com>
2025-08-11 01:01:43 -0600

    Vistas
    Se creo la BD vistas con las vistas a las BD, para observar su informacion desde la BD vistas

---
+++ Fragmentación UAQ/VistaGlobal/Vistas.sql
@@ -0,0 +1,25 @@
+Create database VistaGlobal;
+GO
+
+Use VistaGlobal;
+Go
+
+-- Eliminar la vista si existe
+IF OBJECT_ID('VistaGlobalAlumnos', 'V') IS NOT NULL
+BEGIN
+    DROP VIEW VistaGlobalAlumnos;
+    PRINT 'Vista VistaGlobalAlumnos eliminada para recreación.';
+END
+GO
+
+-- Crear la vista global
+CREATE VIEW VistaGlobalAlumnos AS
+SELECT * FROM CienciasDeLaComputacionUAQ.dbo.Alumnos
+UNION ALL
+SELECT * FROM HumanidadesUAQ.dbo.Alumnos
+UNION ALL
+SELECT * FROM MedicinaUAQ.dbo.Alumnos;
+GO
+
+PRINT 'Vista VistaGlobalAlumnos creada exitosamente.';
+GO
